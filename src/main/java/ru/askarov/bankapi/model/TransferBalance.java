d6d04848c6d6ee92248ada44450921f1b6006eb4
a19552297 <MFAskarov@sberbank.ru>
2021-09-24 12:28:28 +0300

    Добавлена аннотация от LomBok

--- src/main/java/ru/askarov/bankapi/model/TransferBalance.java
+++ src/main/java/ru/askarov/bankapi/model/TransferBalance.java
@@ -1,25 +1,14 @@
 package ru.askarov.bankapi.model;
 
+import lombok.AllArgsConstructor;
+
 import java.math.BigDecimal;
 
+@AllArgsConstructor
 public class TransferBalance {
     private long from;
     private long to;
     private BigDecimal amount;
-
-    public TransferBalance() {
-    }
-
-    public TransferBalance(long from, long to, BigDecimal amount) {
-        this.from = from;
-        this.to = to;
-        this.amount = amount;
-    }
-
-    public TransferBalance(long to, BigDecimal amount) {
-        this.to = to;
-        this.amount = amount;
-    }
 
     public Long getFrom() {
         return from;
