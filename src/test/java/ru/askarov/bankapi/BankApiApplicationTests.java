cd9680539c5309fbcc5482b71a367c151eea60fd
a19552297 <MFAskarov@sberbank.ru>
2021-09-30 09:57:54 +0300

    Удалил лишние строки

--- src/test/java/ru/askarov/bankapi/BankApiApplicationTests.java
+++ src/test/java/ru/askarov/bankapi/BankApiApplicationTests.java
@@ -11,9 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.askarov.bankapi.controller.BalanceController;
 import ru.askarov.bankapi.model.TransferBalance;
-import ru.askarov.bankapi.repository.BalanceRepository;
 
 import java.math.BigDecimal;
 
